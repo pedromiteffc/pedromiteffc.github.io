30619c8df7d445c92e2e89fbb8b4adb2b950c3c3
Pedro Miteff <118797946+pedromiteffc@users.noreply.github.com>
2026-02-25 02:34:27 -0300

    Delete assets directory

--- assets/js/calculator.js
+++
@@ -1,7 +0,0 @@
-const navToggleBtn = document.querySelector('[data-nav-toggle-btn]')
-const header = document.querySelector('[data-header]')
-
-navToggleBtn.addEventListener('click', () =>
-{
-    header.classList.toggle('active')
-})
